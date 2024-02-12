446bdeb81d5887312e1ca510eec4bafa017de612
LukePadawalker <padurariu.luca@gmail.com>
2024-02-12 16:07:44 +0100

    scaffolding + favicon + readme

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+// Supports weights 300-800
+import '@fontsource-variable/open-sans';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app')
