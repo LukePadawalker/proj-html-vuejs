4ccc87e59f965377ff9a0b0a3b74a6f89972db76
LukePadawalker <padurariu.luca@gmail.com>
2024-02-12 17:57:37 +0100

    last update

---
+++ src/components/MainBody/MainCarousel.vue
@@ -0,0 +1,21 @@
+<script>
+// logica carosello
+
+
+
+
+
+</script>
+
+
+
+<template>
+    <TopCarousel />
+    <BottomCarousel />
+</template>
+
+
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
