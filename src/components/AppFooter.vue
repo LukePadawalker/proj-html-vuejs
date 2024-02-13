fc7b9d22bc8143860417cb21051162187c41006a
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-13 11:32:16 +0100

    added footer component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,10 +3,69 @@ import FooterLink from './Footer/FooterLink.vue';
 import FormEmail from './Footer/FormEmail.vue';
 export default {
     name: 'AppFooter',
-    components: { FooterLink, FormEmail }
+    components: { FooterLink, FormEmail },
+    props: {
+        menus: Array
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <div class="container">
+            <div class="top-footer">
+                <div class="footer-row">
+                    <div class="footer-col" v-for="(menu, i) in menus" :key="i">
+                        <FooterLink :title="menu.title" :links="menu.links" />
+                    </div>
+                </div>
+                <FormEmail class="flex-grow-1" />
+            </div>
+            <div class="bottom-footer">
+                <img src="../assets/img/anime-whitelogo.png" alt="white-logo" class="img-fluid">
+                <address class="fs-5"> © Anime Tech, Theme by HasnainDEV</address>
+            </div>
+        </div>
+    </footer>
+</template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+footer {
+    background-color: #bf1d2e;
+    padding: 3rem;
+    color: white;
+    border-top: 5px solid #333333;
+    margin-top: 20px;
+
+    .top-footer {
+        border-bottom: 1px solid white;
+        padding-bottom: 3rem;
+
+        display: flex;
+        align-items: center;
+
+        .footer-row {
+            display: flex;
+            width: 75%;
+
+            .footer-col {
+                flex-basis: calc(100% / 4);
+                gap: 2rem;
+                font-weight: 600;
+            }
+        }
+    }
+
+    .bottom-footer {
+
+        margin-top: 2.5rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        img {
+            max-width: 250px;
+        }
+    }
+}
+</style>
--- src/components/Footer/FooterLink.vue
+++ src/components/Footer/FooterLink.vue
@@ -1,9 +1,32 @@
 <script>
 export default {
-    name: 'FooterLink'
+    name: 'FooterLink',
+    props: {
+        title: String,
+        links: Array
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <h5>{{ title }}</h5>
+    <ul class="p-0">
+        <li v-for="link in links"><a :href="link.url">{{ link.text }}</a></li>
+    </ul>
+</template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+ul li {
+    list-style-type: none;
+    line-height: 1.8;
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
+}
+
+h5 {
+    text-transform: uppercase;
+}
+</style>
