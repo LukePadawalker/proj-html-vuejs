7d290759415ec1a49d3ce26984bd8e4d7e102d78
PasquiFra <francesco.pasquinoni@gmail.com>
2024-02-13 10:52:36 +0100

    wrking on top main branch. filter on gallery

--- src/components/TopMain.vue
+++ src/components/TopMain.vue
@@ -1,15 +1,60 @@
 <script>
-import MainLifestylesVue from './MainBody/TopComps/MainLifestyles.vue';
-import MainNewsVue from './MainBody/TopComps/MainNews.vue';
+import LifestylesCarousel from './MainBody/TopComps/MainLifestyles.vue';
+import { gallery } from '../data/index'
+
 export default {
     name: 'TopMain',
-    components: { MainLifestylesVue, MainNewsVue }
+    data: () => ({
+        ...gallery
+    }),
+    components: { LifestylesCarousel },
+    computed: {
+        galleryFilter() {
+            return filteredGallery = gallery.filter(g => (g.labels.includes("Travel" || "Lifestyle")))
+        }
+    }
 }
 </script>
 
+
 <template>
-    <MainLifestylesVue />
-    <MainNewsVue />
+    <LifestylesCarousel />
+    <section id="display-boxes" class="row">
+        <div class="col-3 d-flex ">
+            <div class="box-s"><img src="" alt=""></div>
+            <div class="box-s"></div>
+        </div>
+        <div class="col-6 box-xl">
+
+        </div>
+        <div class="col-3 d-flex">
+            <div class="box-s"></div>
+            <div class="box-s"></div>
+        </div>
+    </section>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+#display-boxes {
+    height: 300px;
+    background-color: bisque;
+    justify-content: space-between;
+
+    &>div {
+        flex-wrap: wrap;
+        align-content: space-between;
+    }
+}
+
+.box-s {
+    width: 100%;
+    height: 50%;
+    border: 1px solid black;
+}
+
+.box-xl {
+    width: 50%;
+    height: 100%;
+    border: 1px solid black;
+}
+</style>
