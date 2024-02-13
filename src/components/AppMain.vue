f9e762be9b7446d853c22c617d703c6209edac42
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-13 11:55:22 +0100

    added main banner

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,8 +10,26 @@ export default {
 <template>
     <div class="container">
         <TopMain />
+    </div>
+    <div class="banner">
+        <div class="container">
+            <img src="../assets/img/page-banner-1-768x71.webp" alt="banner" class="rounded">
+        </div>
+    </div>
+    <div class="container">
         <BottomMain />
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.banner {
+    padding: 2rem;
+    background-color: #F3F3F3;
+    margin: 3rem 0;
+    text-align: center;
+
+    img {
+        width: 100%;
+    }
+}
+</style>
