b9fab2d5606a120c106e65f347b1900ed297c26e
PasquiFra <francesco.pasquinoni@gmail.com>
2024-02-14 10:43:19 +0100

    initial style for carousel

---
+++ src/components/MainBody/TopComps/CarouselCard.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default {
+    name: 'CarouselCard',
+    props: {
+        src: String,
+        title: String,
+        date: String,
+        labels: Array
+    }
+}
+
+</script>
+
+<template>
+    <div class="card border-0">
+        <img :src="`src/assets/img/${src}`" :alt="title" class="card-img-top">
+        <div id="labels" class="p-4">
+            <a href="#" class="label" v-for="label in labels">{{ label }}</a>
+        </div>
+        <div class="card-body text-center rounded-bottom">
+            <h5 class="card-title fw-bold">{{ title }}</h5>
+            <span class="date text-secondary fw-bold">{{ date }}</span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card-body {
+    background-color: #F3F3F3;
+}
+
+h5:hover {
+    color: #BF1D2E;
+}
+</style>
