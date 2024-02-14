4b95e7400606f17900682cf5594d8d588db22b27
PasquiFra <francesco.pasquinoni@gmail.com>
2024-02-14 09:50:36 +0100

    start on main carousel

---
+++ src/components/MainBody/TopComps/CarouselCard.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+    name: 'CarouselCard',
+    props: {
+        src: String,
+        title: String,
+        date: String,
+    }
+}
+
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
