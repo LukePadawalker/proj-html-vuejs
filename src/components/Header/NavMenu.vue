1d7671e3c68e28f1334d6fe1bca4b7a969f4bdf6
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-02-15 12:07:57 +0100

    fixed offcanvas

--- src/components/Header/NavMenu.vue
+++ src/components/Header/NavMenu.vue
@@ -25,13 +25,26 @@ export default {
         </div>
         <div class="offcanvas-body">
             <img class="offcanvas-logo" src="../../assets/img/anime-logo.webp" alt="">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolores maiores ullam aliquam consectetur rerum.
+            <p class="px-3 fw-medium fs-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolores maiores
+                ullam
+                aliquam
+                consectetur rerum.
             </p>
             <picture class="offcanvas-pics d-flex">
                 <img src="../../assets/img/music-love.webp" alt="">
                 <img src="../../assets/img/best-places.webp" alt="">
                 <img src="../../assets/img/anime-fashion.webp" alt="">
             </picture>
+            <div class="contacts">
+                <ul class="list-unstyled d-flex align-items-center py-2 ps-4 mb-0">
+                    <li><a href="#" class="text-decoration-none"><i class="fa-solid fa-clock me-2"></i>Sun - Sat : 9:00 AM -
+                            18:00 PM</a></li>
+                    <li><a href="#" class="text-decoration-none"><i
+                                class="fa-solid fa-envelope me-2"></i>contact@domain.com</a></li>
+                    <li><a href="#" class="text-decoration-none"><i class="fa-solid fa-phone-volume me-2"></i>(+82) 8123 456
+                            789</a></li>
+                </ul>
+            </div>
             <div class="socials">
                 <ul class="list-unstyled d-flex align-items-center py-2 ps-4 mb-0">
                     <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
@@ -50,7 +63,7 @@ export default {
     position: fixed;
     top: 0;
     bottom: 0;
-    left: 45vw;
+    left: 30vw;
     right: 0;
     z-index: 1;
     background-color: rgba(0, 0, 0, 0.7);
@@ -65,7 +78,7 @@ a {
 }
 
 #offcanvasMenu {
-    width: 45vw;
+    width: 30vw;
 
     .offcanvas-header {
         position: relative;
@@ -75,42 +88,44 @@ a {
             top: 0px;
             left: 100%;
 
-            width: 50px;
+            width: 65px;
             height: 50px;
-            background-color: rgb(94, 93, 93);
+            background-color: #BF1D2E;
+            z-index: 2;
+            cursor: pointer;
 
             i.fa-xmark {
+                font-size: 16px;
                 position: absolute;
                 top: 50%;
                 left: 50%;
                 translate: -50% -50%;
-                z-index: 2;
-
                 color: white;
-
-                padding: 10px;
-                border: 2px solid white;
+                padding: 2px 4px 2px 5px;
+                border: 3px solid white;
+                border-radius: 4px;
+                background-color: #BF1D2E
             }
         }
     }
 
     .offcanvas-body {
-        padding: 50px 30px;
+        padding: 140px 40px;
         text-align: center;
-
 
         .offcanvas-logo {
             width: 300px;
             height: auto;
-            margin: 50px 0;
+            margin: 30px 0;
         }
 
 
         picture {
-            margin: 50px 0;
             width: 100%;
-            height: 125px;
+            height: 110px;
             justify-content: center;
+            margin-bottom: 20px;
+            margin-top: 30px;
 
             img {
                 border-radius: 10px;
@@ -126,6 +141,8 @@ a {
     display: flex;
     align-items: center;
     justify-content: center;
+    position: relative;
+    right: 15px;
 
     ul {
         list-style: none;
@@ -138,18 +155,59 @@ a {
 
             i {
                 background-color: #bf1d2e;
-                width: 40px;
-                height: 40px;
-                font-size: 16px;
+                width: 35px;
+                height: 35px;
+                font-size: 15px;
                 border-radius: 50%;
                 color: #ffffff;
                 text-align: center;
-                line-height: 40px;
+                line-height: 35px;
                 transition: color 0.3s, background-color 0.4s;
 
                 &:hover {
                     color: #fff;
                     background-color: #545454;
+                }
+            }
+        }
+    }
+}
+
+.contacts {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    right: 15px;
+    margin-bottom: 20px;
+
+    ul {
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        li {
+            cursor: pointer;
+            line-height: 30px;
+            font-weight: 500;
+
+            i {
+
+                font-size: 16px;
+                color: #bf1d2e;
+                text-align: center;
+                transition: color 0.3s, background-color 0.4s;
+
+            }
+
+            &:hover {
+                i {
+                    color: #545454;
+                }
+
+                a {
+                    color: #BF1D2E;
                 }
             }
         }
